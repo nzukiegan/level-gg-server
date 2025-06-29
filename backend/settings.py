af59c9cbe4bc9c89826a4c9561e87c6baf623fe9
nzukiegan <nzukiegan6@gmail.com>
2025-06-29 15:46:51 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-da)pj4k5!ih3n8g52exap281$^l+1w6u42ysvxi0bu5pul_nuv
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['level-gg-server.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['level-gg-server.onrender.com', 'localhost', '127.0.0.1', 'https://level-gg-client.onrender.com']
 
 AUTH_USER_MODEL = 'tournaments.Player'
 
