bc9c0012b982e6ac928bfb54293af4a58ba7fc7f
nzukiegan <nzukiegan6@gmail.com>
2025-06-30 14:11:08 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_URL'),
-    os.environ.get('BACKEND_URL'),
+    os.environ.get('BACKEND_URL2'),
 ]
 
 CORS_ALLOW_CREDENTIALS = True
